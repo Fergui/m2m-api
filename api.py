72adbed6dfd1f07bfc26585bb67d01ab667a7878
Angel Farguell Caus <angel.farguell@gmail.com>
2021-02-16 09:02:32 -0700

    first download capability

--- api.py
+++ api.py
@@ -7,8 +7,10 @@ import os.path as osp
 from getpass import getpass
 
 from .filters import Filter
+from .downloader import download_url
 
 M2M_ENDPOINT = 'https://m2m.cr.usgs.gov/api/api/json/{}'
+ACQ_PATH = './ingest'
 
 class M2MError(Exception):
     """
@@ -87,7 +89,57 @@ class M2M(object):
             args['datasetFilters'] = self.datasetFilters(**args)
         args['processList'] = ['datasetName','sceneFilter','maxResults']
         params = Filter(args)
-        return self.sendRequest('scene-search', params)
+        scenes = self.sendRequest('scene-search', params)
+        if scenes['totalHits'] > scenes['recordsReturned']:
+            logging.warning('M2M.searchScenes - more hits {} than returned records {}, consider increasing maxResults parameter.'.format(scenes['totalHits'],
+                                                                                                                                        scenes['recordsReturned']))
+        return scenes
+
+    def downloadOptions(self, datasetName, entityIds, filterOptions={}):
+        if datasetName not in self.datasetNames:
+            raise M2MError("Dataset {} not one of the available datasets {}".format(datasetName,self.datasetNames))
+        params = {'datasetName': datasetName, 
+                'entityIds': entityIds}
+        downloadOptions = self.sendRequest('download-options', params)
+        filteredOptions = apply_filter(downloadOptions,filterOptions)
+        return filteredOptions
+            
+    def downloadRequest(self, downloadList, label='m2m-api_download'):
+        params = {'downloads': downloadList,
+                'label': label}
+        return self.sendRequest('download-request', params)
+
+    def downloadRetrieve(self, label='m2m-api_download'):
+        params = {'label': label}
+        return self.sendRequest('download-retrieve', params)
+
+    def downloadSearch(self, label='m2m-api_download'):
+        params = {'label': label}
+        return m2m.sendRequest('download-search', params)
+
+    def retrieveScenes(self, scenes, label='m2m-api_download'):
+        entityIds = [scene['entityId'] for scene in scenes['results']]
+        filterOptions = {'downloadSystem': lambda x: x == 'dds_zip', 'available': lambda x: x}
+        downloadOptions = self.downloadOptions(params['datasetName'], entityIds, filterOptions)
+        downloads = [{'entityId' : product['entityId'], 'productId' : product['id']} for product in downloadOptions]
+        requestResults = self.downloadRequest(downloads)
+        downloadMeta = {}
+        for ds in m2m.downloadSearch(label):
+            downloadMeta.update({str(ds['downloadId']): ds})
+        if requestResults['preparingDownloads'] != None and len(requestResults['preparingDownloads']) > 0:
+            requestResultsUpdated = self.downloadRetrieve(label)
+        else:
+            for download in requestResults['availableDownloads']:
+                idD = str(download['downloadId'])
+                displayId = downloadMeta[idD]['displayId']
+                url = download['url']
+                local_path = osp.join(ACQ_PATH,displayId+'.tar')
+                if not available_locally(local_path):
+                    download_url(url, local_path)
+                downloadMeta[idD].update({'url': url, 'local_path': local_path})
+                downloadMeta[idD]['statusCode'] = 'C'
+                downloadMeta[idD]['statusText'] = 'Complete'
+        return downloadMeta
 
     def logout(self):
         r = self.sendRequest('logout')
@@ -111,3 +163,26 @@ def retry_connect(url, json_data, headers={}, max_retries=5, sleep_seconds=1):
         else:
             return response
     raise M2MError("Maximum retries exceeded")
+
+def apply_filter(elements, key_filters):
+    result = []
+    for element in elements:
+        get_elem = True
+        for key,filt in key_filters.items():
+            if not filt(element[key]):
+                get_elem = False
+        if get_elem:
+            result.append(element)
+    return result
+
+def available_locally(path):
+    """
+    Check if a file is available locally and if it's file size checks out.
+
+    :param path: the file path
+    """
+    info_path = path + '.size'
+    if osp.exists(path) and osp.exists(info_path):
+        content_size = int(open(info_path).read())
+        return osp.getsize(path) == content_size
+    return False
---
+++ downloader.py
@@ -0,0 +1,79 @@
+import logging, time, subprocess, requests, random, os
+from six.moves.urllib import request as urequest
+import os.path as osp
+
+sleep_seconds = 5
+max_retries = 3
+wget = 'wget'
+wget_options = ["--read-timeout=1"]
+download_sleep_seconds = 3
+
+class DownloadError(Exception):
+    """
+    Raised when the downloader is unable to retrieve a URL.
+    """
+    pass
+
+def download_url(url, local_path, content_size, max_retries=max_retries, sleep_seconds=sleep_seconds):
+    """
+    Download a remote URL to the location local_path with retries.
+
+    On download, the file size is first obtained and stored.  When the download completes,
+    the file size is compared to the stored file.  This prevents broken downloads from
+    contaminating the processing chain.
+
+    :param url: the remote URL
+    :param local_path: the path to the local file
+    :param max_retries: how many times we may retry to download the file
+    :param sleep_seconds: sleep seconds between retries
+    """
+    logging.info('download_url - {0} as {1}'.format(url, local_path))
+    logging.debug('download_url - if download fails, will try {0} times and wait {1} seconds each time'.format(max_retries, sleep_seconds))
+    sec = random.random() * download_sleep_seconds
+    logging.info('download_url - sleeping {} seconds'.format(sec))
+    time.sleep(sec)
+
+    remove(local_path)
+    command=[wget,'-O',ensure_dir(local_path),url]
+    for opt in wget_options:
+        command.insert(1,opt)
+    logging.info(' '.join(command))
+    subprocess.call(' '.join(command),shell=True)
+
+    file_size = osp.getsize(local_path)
+
+    logging.info('download_url - local file size {0} remote content size {1}'.format(file_size, content_size))
+
+    if int(file_size) != int(content_size):
+        logging.warning('download_url - wrong file size, trying again, retries available {}'.format(max_retries))
+        if max_retries > 0:
+            logging.info('download_url - sleeping {} seconds'.format(sleep_seconds))
+            time.sleep(sleep_seconds)
+            download_url(url, local_path, content_size, max_retries = max_retries-1)
+            return
+        else:
+            os.remove(local_path)
+            raise DownloadError('download_url - failed to download file {}'.format(url))
+
+    info_path = local_path + '.size'
+    open(ensure_dir(info_path), 'w').write(str(content_size))
+
+def ensure_dir(path):
+    """
+    Ensure all directories in path if a file exist, for convenience return path itself.
+
+    :param path: the path whose directories should exist
+    :return: the path back for convenience
+    """
+    path_dir = osp.dirname(path)
+    if not osp.exists(path_dir):
+        os.makedirs(path_dir)
+    return path
+
+def remove(tgt):
+    """
+    os.remove wrapper
+    """
+    if osp.isfile(tgt):
+        logging.info('remove - file {} exists, removing'.format(tgt))
+        os.remove(tgt)
